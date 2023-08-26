8b378dc10c27b7d7b9a5f010a40ee4f2b112b9fc
edmoore3 <edmoore3@asu.edu>
2023-08-26 10:06:31 -0700

    Added wikipedia functionality

--- reading_time/scripts/content.js
+++ reading_time/scripts/content.js
@@ -1,7 +1,8 @@
-const article = document.querySelector("article");
+let article = document.querySelector("article");
 
 // document.querySelector may return null
-if (article) {
+if (article)
+{
     const text = article.textContent;
     const wordMatchRegExp = /[^\s]+/g;
     const words = text.matchAll(wordMatchRegExp);
@@ -13,10 +14,44 @@ if (article) {
     badge.classList.add("color-secondary-text", "type--caption");
     badge.textContent = `⏱️ ${readingTime} min read`;
 
+    console.log("word count = " + wordCount);
     // Support for API reference docs
-    const heading = article.querySelector("h1");
+    const heading = article.querySelector("div");
     // Support for article docs with date
     const date = article.querySelector("time")?.parentNode;
 
+    if(date)
+    {
+        console.log("DATE");
+    }
+
     (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+else // Wikipedia
+{
+    //article = document.getElementsByClassName("mw-parser-output")[0];
+    article = document.querySelector("main");
+
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g;
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+    let wordCount = 0;
+
+    for( let word of words )
+    {
+        wordCount += (word[0][0] != "."); // Ignores meta words
+    }
+
+    const readingTime = Math.round(wordCount / 200); // assume 200 words per min
+    const badge = document.createElement("p");
+    
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
+
+    // Get title of article
+    const heading = article.querySelector("header");
+
+    heading.insertAdjacentElement("afterend", badge);
 }
